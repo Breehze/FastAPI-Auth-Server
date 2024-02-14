0281a6da9f27cad9b355a7cbcd61e45262d93564
Breehze <boris.ftorek@gmail.com>
2024-02-14 21:43:22 +0100

    Authorization code flow -sleeper version

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import fastapi
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates 
-import cryptostuff
+from fastapi.templating import Jinja2Templates
+from urllib.parse import urlencode 
+from cryptostuff import CodeManager, JWTmanager
 import  asyncio
 from pydantic import BaseModel 
 
@@ -11,14 +12,16 @@ test_codes = {}
 
 app = fastapi.FastAPI()
 
-code_manager = cryptostuff.CodeManager(test_codes)
+code_manager = CodeManager(test_codes)
+
+jwt_manager = JWTmanager()
 
 app.mount("/static",StaticFiles(directory = "static"), name = "static")
 
 templates = Jinja2Templates(directory="templates")
 
 
-test_clients = {"someclient" : "http://example.com/callback"}
+test_clients = {"someclient" : "https://example.com/callback"}
 
 test_users = {"boris" : {"password" : "hello"}}
 
@@ -29,72 +32,54 @@ class AuthGrantBody(BaseModel):
     client_id : str 
     client_secret : str | None = None
 
-
-
-
 @app.on_event("startup")
 async def startup_events():
     c_m = asyncio.create_task(code_manager.manage())
     print("Startup complete")
     asyncio.gather(c_m)
 
-
+@app.get("/auth")
+async def authentification_page(request : fastapi.Request,response_type : str = "code", client_id : str = None, redirect_uri : str = None, state : str = None):
+    q_str = urlencode(dict(request.query_params))
+    login_p_endp = "/login?"+q_str
+    return templates.TemplateResponse("login.html",{"request": request,"url" : login_p_endp})
 
 @app.post("/token")
 async def exchange_token(auth_grant_body : AuthGrantBody):
     if auth_grant_body.grant_type != "authorization_code":
-        return {"Error" : "I do not support this flow"}
+        raise fastapi.HTTPException(status_code=400, detail= "Authorization flow not supported")
     if code_manager.validate_code(auth_grant_body.code) is False:
-        return {"Error" : "Invalid/Expired token"}
+        raise fastapi.HTTPException(status_code=400, detail= "Invalid or Expired token")
     if code_manager.validate_url(auth_grant_body.code,auth_grant_body.redirect_uri) is False:
-        return {"Error" : "Invalid redirect url"}
+        raise fastapi.HTTPException(status_code=400, detail= "Invalid redirect url")
     if auth_grant_body.client_id not in test_clients:
-        return {"Error" : "This client app does not exist"}
-
+        raise fastapi.HTTPException(status_code=400, detail= "Client does not exist")
     
-    return {"Hello" : auth_grant_body.grant_type}
+    print(auth_grant_body.redirect_uri)
+    token = jwt_manager.jwt_get(sub= test_codes[auth_grant_body.code]['associated_user']  , aud = auth_grant_body.redirect_uri)
+    print(jwt_manager.jwt_decode(token,aud=auth_grant_body.redirect_uri))
+    test_codes.pop(auth_grant_body.code)
+    
+    return {"access_token" : token, "token_type" : "bearer"}
 
-@app.get("/login")
-async def login_proc(request : fastapi.Request,response_type : str = "code", client_id : str = None, redirect_uri : str = None, state : str = None):
-    return templates.TemplateResponse("login.html",{"request": request})
 
 @app.post("/login")
 async def login(form_data : OAuth2PasswordRequestForm = fastapi.Depends(),response_type : str = "code", client_id : str = None, redirect_uri : str = None, state : str = None):
     if form_data.username not in test_users or form_data.password != test_users[form_data.username]["password"] :
-        return fastapi.HTTPException(status_code=401, detail= "Incorect password or username")
+        raise fastapi.HTTPException(status_code=401, detail= "Incorect password or username")
     if client_id not in test_clients or client_id is None:
-        return fastapi.HTTPException(status_code=400, detail= "Client does not exist")
+        raise fastapi.HTTPException(status_code=400, detail= "Client does not exist")
     if redirect_uri != test_clients["someclient"] or redirect_uri is None:
-        return fastapi.HTTPException(status_code=400, detail= "Redirect URI not valid")
+        raise fastapi.HTTPException(status_code=400, detail= "Redirect URI not valid")
 
-    authorization_code = cryptostuff.url_code()
+    authorization_code = code_manager.url_code()
 
     uri_tempered = f"{redirect_uri}?code={authorization_code}"
 
     if state is not None:
         uri_tempered += f"&state={state}"
 
-    test_codes.update({authorization_code : {"issue_time" : cryptostuff.issuance_time(), "associated_url" : redirect_uri} })
+    test_codes.update({authorization_code : {"issue_time" : code_manager.issuance_time(), "associated_url" : redirect_uri,"associated_user" : form_data.username}})
 
     return fastapi.responses.RedirectResponse(uri_tempered,status_code=303)
-
-
-@app.get("/auth")
-async def authorization(response_type : str = "code", client_id : str = None, redirect_uri : str = None, state : str = None):
-    if client_id not in test_clients or client_id is None:
-        return {"Error" : "Client does not exist"}
-    if redirect_uri != test_clients["someclient"] or redirect_uri is None:
-        return {"Error": "Invalid uri"}
-
-    authorization_code = cryptostuff.url_code()
-
-    uri_tempered = f"{redirect_uri}?code={authorization_code}"
-
-    if state is not None:
-        uri_tempered += f"&state={state}"
-
-    test_codes.update({authorization_code : {"issue_time" : cryptostuff.issuance_time(), "associated_url" : redirect_uri} })
-
-    return fastapi.responses.RedirectResponse(uri_tempered)
-
 
