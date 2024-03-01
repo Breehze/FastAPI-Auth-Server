794d205ce196123017391f53e0a116fa0959c6fd
Breehze <boris.ftorek@gmail.com>
2024-03-01 17:53:23 +0100

    Added CORS middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from asyncio.tasks import create_task
 from fastapi import FastAPI 
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth_flow, user_related
 from contextlib import asynccontextmanager
 import asyncio
@@ -22,6 +23,15 @@ async def lifespan(app : FastAPI):
 
 
 app = FastAPI(lifespan = lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+
+)
 
 app.mount("/static",StaticFiles(directory = "static"), name = "static")
 
